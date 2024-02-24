5afae052167d41febd4d7e6f89aeb2e91427a463
Mercyodhiambo <amercy2466@gmail.com>
2024-02-24 13:26:43 +0300

    add more data

--- Forloop.py
+++ Forloop.py
@@ -2,7 +2,7 @@
 # With loops we work with a list of items
 # => List (['Wamae', 'Mercy'], [1, 2, 3, 4], [{name: "Wamae", reg_no: "E46/6272/2021"}, {name: "Mercy", reg_no: "E46/6902/2021"}])
 
-namesList = ['Wamae', 'Mercy']
+namesList = ['Wamae', 'Mercy', 'APHLINE', "Moses", "cindy"]
 
 for name in namesList:
     print(name)
