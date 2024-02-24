fa88d9c311fdca967e658b7de6a645bd5a8ca913
Mercyodhiambo <amercy2466@gmail.com>
2024-02-24 13:21:08 +0300

    initialized loops

---
+++ Forloop.py
@@ -0,0 +1,8 @@
+# Loop through items and perform actions on each
+# With loops we work with a list of items
+# => List (['Wamae', 'Mercy'], [1, 2, 3, 4], [{name: "Wamae", reg_no: "E46/6272/2021"}, {name: "Mercy", reg_no: "E46/6902/2021"}])
+
+namesList = ['Wamae', 'Mercy']
+
+for name in namesList:
+    print(name)
